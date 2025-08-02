f497d866685b1ce286a8a03145003cdf5c721b23
ritesh <patilritesh998@gmail.com>
2025-08-02 11:17:25 +0530

    Portfolio

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,10 +4,10 @@ function Footer() {
   return (
 <div>
     <center>
-      <p className='text-sm center'>© 2023 Ritesh Patil. All rights reserved.</p>
-      </center>
+      <p className='text-sm center bg-[rgb(239,238,242)] p-5' >© 2023 Ritesh Patil. All rights reserved.</p>
 
-    </div>
+      </center>
+</div>
   )
 }
 
--- src/pages/Aboutus.jsx
+++ src/pages/Aboutus.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 
+
 function Aboutus() {
   return (
-    <div>Aboutus</div>
-  )
+    <div className="md:bg-[url('/public/bg.jpg')] md:h-170 md:w-full md:bg-cover " >
+    
+      <div className='md:pr-150 md:pl-15 pt-10'>
+<h1 className='font-serif text-5xl md:font-lightbold'>About <span className='text-[rgb(245,195,85)]'>Me </span></h1>
+<p className=' pt-6 md:pr-4 md:font-serif md:text-xl italic'>Hi 👋, I’m Ritesh Patil — a 3rd-year Diploma student in Information Technology 💻 at K. K. Wagh Polytechnic, Nashik 📍. I’m passionate about leveraging technology to solve real-world problems 🌍, with a special interest in web development 🌐, Java programming ☕, and IoT-based solutions 📡.
+I love exploring new tech trends 🚀, building innovative projects 🛠️, and continuously upgrading my skills to stay ahead in the digital era 📈. My goal is to create impactful solutions that make life easier and smarter 🤖. Always curious and eager to learn, I believe in turning ideas into reality 💡.
+</p>
+<h1 className='pt-6 text-2xl '>💻 Technical Skills:</h1>
+<h1 className='pt-2 md:text-xl italic '>Languages: Java, C, C++</h1>
+<h1 className='pt-2 md:text-xl italic'>Web: HTML, CSS, React.js, Tailwind CSS</h1>
+<h1 className='pt-2 md:text-xl italic'>Databases: MySQL, Firebase, MongoDB</h1>
+<h1 className='pt-2 md:text-xl italic'>Tools: Eclipse, Node.js, Flask</h1>
+<p className='pt-2 md:text-xl italic'>🏋️ Beyond Tech:
+When I’m not coding, you’ll often find me in the gym, exploring mythology, or experimenting with creative side projects.</p>
+<p className='pt-2 md:text-xl italic mb-3'>🎯 Goal:
+To become a skilled full-stack developer and create innovative solutions that make technology more impactful in everyday life.</p>
+      </div>
+      </div>
+  );
 }
 
-export default Aboutus
+export default Aboutus;
+
+
