4ff0983e8cebcf52625e02285d8bf5c797b996a1
ritesh <patilritesh998@gmail.com>
2025-08-03 13:50:22 +0530

    Portfolio

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,10 +5,10 @@ function Navbar() {
     <div className='bg-[rgb(245,195,85)] text-black md:p-8 p-4'>
       <div className='container mx-auto md:flex flex-col md:flex-row justify-end items-center md:gap-10 '>
         {/* Navigation Links */}
-        <a href="/Homepage" className='mb:block mb:text-center md:text-lg font-bold md: md:font-normal hover:underline pr-7'>Homepage</a>
-        <a href="/Aboutus" className='mb:block mb:text-center md:text-lg font-bold md: md:font-normal hover:underline pr-7'>About Us</a>
-        <a href="/Projects" className='mb:block mb:text-center md:text-lg font-bold  md:font-normal hover:underline pr-7'>Projects</a>
-        <a href="/Contact" className='mb:block mb:text-center md:text-lg font-bold md:font-normal hover:underline '>Contact</a>
+        <a href="/Homepage" className='mb:block mb:text-center md:text-lg md: md:font-normal hover:underline pr-7'>Homepage</a>
+        <a href="/Aboutus" className='mb:block mb:text-center md:text-lg  md: md:font-normal hover:underline pr-7'>About Us</a>
+        <a href="/Projects" className='mb:block mb:text-center md:text-lg  md:font-normal hover:underline pr-7'>Projects</a>
+        <a href="/Contact" className='mb:block mb:text-center md:text-lg  md:font-normal hover:underline '>Contact</a>
       </div>
     </div>
   );
