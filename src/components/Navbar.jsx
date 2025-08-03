7d87140618a3f8fe59c9c092a0b1efc14ee341df
ritesh <patilritesh998@gmail.com>
2025-08-03 11:07:41 +0530

    Portfolio

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,16 +2,17 @@ import React from 'react';
 
 function Navbar() {
   return (
-    <div className='bg-[rgb(245,195,85)] text-black md:p-8'> {/* Adjust padding for all screens */}
-      <div className='container mx-auto flex flex-col md:flex-row justify-end items-center gap-4 md:gap-10'>
+    <div className='bg-[rgb(245,195,85)] text-black md:p-8 p-4'>
+      <div className='container mx-auto md:flex flex-col md:flex-row justify-end items-center md:gap-10 '>
         {/* Navigation Links */}
-        <a href="/Homepage" className='block text-center text-lg font-bold md: md:font-normal hover:underline'>Homepage</a>
-        <a href="/Aboutus" className='block text-center text-lg font-bold md: md:font-normal hover:underline'>About Us</a>
-        <a href="/Projects" className='block text-center text-lg font-bold  md:font-normal hover:underline'>Projects</a>
-        <a href="/Contact" className='block text-center text-lg font-bold md:font-normal hover:underline'>Contact</a>
+        <a href="/Homepage" className='mb:block mb:text-center md:text-lg font-bold md: md:font-normal hover:underline pr-7'>Homepage</a>
+        <a href="/Aboutus" className='mb:block mb:text-center md:text-lg font-bold md: md:font-normal hover:underline pr-7'>About Us</a>
+        <a href="/Projects" className='mb:block mb:text-center md:text-lg font-bold  md:font-normal hover:underline pr-7'>Projects</a>
+        <a href="/Contact" className='mb:block mb:text-center md:text-lg font-bold md:font-normal hover:underline '>Contact</a>
       </div>
     </div>
   );
 }
 
 export default Navbar;
+
--- src/pages/Aboutus.jsx
+++ src/pages/Aboutus.jsx
@@ -3,11 +3,12 @@ import React from 'react'
 
 function Aboutus() {
   return (
-    <div className="md:bg-[url('/public/bg.jpg')] md:h-170 md:w-full md:bg-cover " >
+    <div className="md:bg-[url('/public/bg.jpg')] md:h-170 md:w-full md:bg-cover w-full bg-[url('/src/assets/Image/Mobilebg.jpeg')] bg-cover">
+
     
-      <div className='md:pr-150 md:pl-15 pt-10'>
-<h1 className='font-serif text-5xl md:font-lightbold'>About <span className='text-[rgb(245,195,85)]'>Me </span></h1>
-<p className=' pt-6 md:pr-4 md:font-serif md:text-xl italic'>Hi 👋, I’m Ritesh Patil — a 3rd-year Diploma student in Information Technology 💻 at K. K. Wagh Polytechnic, Nashik 📍. I’m passionate about leveraging technology to solve real-world problems 🌍, with a special interest in web development 🌐, Java programming ☕, and IoT-based solutions 📡.
+      <div className='md:pr-150 md:pl-15 pt-10 pr-7 pl-7'>
+<h1 className='font-serif md:text-5xl text-4xl md:font-lightbold'>About <span className='text-[rgb(245,195,85)]'>Me </span></h1>
+<p className=' pt-6 md:pr-4 md:font-serif md:text-xl italic '>Hi 👋, I’m Ritesh Patil — a 3rd-year Diploma student in Information Technology 💻 at K. K. Wagh Polytechnic, Nashik 📍. I’m passionate about leveraging technology to solve real-world problems 🌍, with a special interest in web development 🌐, Java programming ☕, and IoT-based solutions 📡.
 I love exploring new tech trends 🚀, building innovative projects 🛠️, and continuously upgrading my skills to stay ahead in the digital era 📈. My goal is to create impactful solutions that make life easier and smarter 🤖. Always curious and eager to learn, I believe in turning ideas into reality 💡.
 </p>
 <h1 className='pt-6 text-2xl '>💻 Technical Skills:</h1>
